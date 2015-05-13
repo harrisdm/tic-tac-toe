2de434e1db11c287e787af6b12a0953f8d9e7720
Mark Harris <harrisdm@hotmail.com>
2015-05-13 23:54:20 +1000

    Super T-T-T working with Play again - human only

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -121,7 +121,7 @@ $(document).ready( function() {
 
       // Decide if someone has won
       gameOver = isGameOver();
-
+      //console.log("G.O: "+gameOver);
       return true;
     }
     return false;
@@ -169,18 +169,72 @@ $(document).ready( function() {
     Animation for when a player wins
   ******************************************/
   var winAnimation = function() {
+    
+    //assignSquare($("#gameBoardContainer"), currentPlayer);
+    //$("#gameBoardContainer").children().hide();
+    
     alert("Game Won!!");
-    $("#playAgain").slideDown(500);
+    
+    //$("#playAgain").slideDown(500);
+    
     // Update the score board
-    //updateScores();
+    updateScores();
+  };
+
+
+  /*****************************************
+     Check if the main game won
+  *****************************************/
+  var isWin = function() {
+    //console.log("isWin");
+    // What are the possible win states
+    var winStates = [ [1,2,3], [4,5,6], [7,8,9], 
+                      [1,4,7], [2,5,8], [3,6,9],
+                      [1,5,9], [3,5,7] ];
+
+
+    var $mainSquares = $(".gameBoard");
+    // Check the board against each of the win states
+    for(var i = 0; i < winStates.length; i++) {
+
+      var square_1 = $mainSquares.filter("."+winStates[i][0]).data("player");
+      var square_2 = $mainSquares.filter("."+winStates[i][1]).data("player");
+      var square_3 = $mainSquares.filter("."+winStates[i][2]).data("player");
+
+       // console.log("1: "+square_1);
+       // console.log("2: "+square_2);
+       // console.log("3: "+square_3);
+
+       // Ensure its not matching unselected game squares
+      if ( square_1 !== "" && square_1 !== "draw") {
+        // Check that the win state elements match
+        if ( square_1 === square_2 && square_1 === square_3 ) {
+
+          if ( square_1 === "X" ) {
+            winsX++;
+          } else {
+            winsO++;
+          }
+
+          
+
+          gameCounter++;
+
+          // Delay WIN annimation until after the tile has finished turning
+          window.setTimeout(winAnimation, 1000);
+          return true;
+        }
+      }
+    }
+    return false;
   };
 
 
   /******************************************
-    Check if the move won a game
+    Check if a sub game won
   ******************************************/
-  var isWin = function() {
-    console.log("checkWin");
+  var isWinSub = function() {
+    //console.log("isWinSub");
     // What are the possible win states
     var winStates = [ [1,2,3], [4,5,6], [7,8,9], 
                       [1,4,7], [2,5,8], [3,6,9],
@@ -202,19 +256,10 @@ $(document).ready( function() {
         // Check that the win state elements match
         if ( square_1 === square_2 && square_1 === square_3 ) {
           $element.siblings().andSelf().hide();
+          //console.log("parent: "+$element.parent());
           assignSquare($element.parent(), currentPlayer);
           masterMoves++;
-          
-          // if ( square_1 === "X" ) {
-          //   winsX++;
-          // } else {
-          //   winsO++;
-          // }
-
-          // gameCounter++;
-
-          // Delay WIN annimation until after the tile has finished turning
-          window.setTimeout(winAnimation, 1000);
+ 
           return true;
          }
       }
@@ -227,30 +272,54 @@ $(document).ready( function() {
     Animation for when a game draws
   ******************************************/
   var drawAnimation = function() {
-     alert("DRAW: No more moves left");
-  //   $("#playAgain").slideDown(500);
-  //   // Update the score board
-  //   //updateScores();
+
+    //$element.parent().siblings().andSelf().hide();
+    //assignSquare($("#gameBoardContainer"), "draw");
+
+    alert("DRAW: No more moves left");
+
+    //$("#playAgain").slideDown(500);
+
+    // Update the score board
+    updateScores();
   };
 
 
   /*****************************************
-     Check if the game is a draw
+     Check the main game is a draw
   *****************************************/
   var isDraw = function() {
-    console.log("isDraw");
-    console.log(subMoves);
+    //console.log("isDraw");
+    //console.log(masterMoves);
     // Have all squares been selected but no win
-    if ( !gameOver && subMoves.indexOf(9) !== -1 ) {
+    //var gameBoard = [$element.parent().attr('class').split(' ')[0] - 1]
+    if ( !gameOver && masterMoves === 9 ) {
+
+      gameCounter++;
+
+      // Delay DRAW annimation until after the tile has finished turning
+      window.setTimeout(drawAnimation, 1000);
+      return true;
+    }
+    return false;
+   };
+
+
+  /*****************************************
+     Check if a sub game is a draw
+  *****************************************/
+  var isDrawSub = function() {
+    //console.log("isDrawSub");
+    //console.log(subMoves);
+    // Have all squares been selected but no win
+    var gameBoard = [$element.parent().attr('class').split(' ')[0] - 1]
+    if ( !gameOver && subMoves[gameBoard] === 9 ) {
 
       $element.siblings().andSelf().hide();
       assignSquare($element.parent(), "draw");
 
+      masterMoves++;
 
-  //     gameCounter++;
-
-      // Delay DRAW annimation until after the tile has finished turning
-      //window.setTimeout(drawAnimation, 1500);
       return true;
     }
     return false;
@@ -261,16 +330,66 @@ $(document).ready( function() {
     Check if the game is over
   ******************************************/
   var isGameOver = function() {
-    console.log("checkGameOver");
+    //console.log("checkGameOver");
     // Has either game termination critera been met
     // checkWin() must go first in case the final move results in a win
-    return ( isWin() || isDraw() ) ? true : false;
+    if ( isSuperGame && (isWinSub() || isDrawSub()) ) {
+      return ( isWin() || isDraw() ) ? true : false;
+    } else {
+      return false;
+    }
 
 
   };
 
 
 
+
+
+
+
+  /******************************************
+    Update the scoreboard when a player wins
+  ******************************************/
+  var updateScores = function() {
+    $("#gameCount").html(gameCounter);
+    $("#scoreX").html(winsX);
+    $("#scoreO").html(winsO);
+  };
+
+  var resetBoard = function() {
+    $(".gameBoard, .gameSquare").off("click")
+                                .removeClass("X")
+                                .removeClass("O")
+                                .removeClass("draw")
+                                .data("player","")
+                                .show();
+     totalMoves = 0;
+     subMoves = [0,0,0,0,0,0,0,0,0];
+     masterMoves = 0;
+
+     gameOver = false;
+     gameStop = false;
+
+     getMove();
+  };
+
+  /******************************************
+    Reset the board for another game
+  ******************************************/
+  var confirmResetBoard = function () {
+    //console.log("Game Over: " + gameOver);
+    if(gameOver) {
+      //console.log("game over, do stuff");
+      resetBoard();
+
+      //$("#playAgain").slideUp(500);
+
+    } else if(confirm("Are you sure you want to discard this game?")) {
+      resetBoard();
+    }
+  };
+  $("#playAgain").on("click", confirmResetBoard);
 
 
 
@@ -305,6 +424,7 @@ $(document).ready( function() {
     Control who makes the next move
   ******************************************/
   var getMove = function() {
+    console.log("make a move");
     if ( !gameOver && !gameStop ) {
       currentPlayer = findNextPlayer();
       if ( currentPlayer === "X" ) {
@@ -313,6 +433,8 @@ $(document).ready( function() {
         selectPlayerInput(inputTypeO);
       }
     }
+
+    //console.log("Last Game Over: " + gameOver);
   };
 
 getMove();
